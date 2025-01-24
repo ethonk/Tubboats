7e1a5394cd3af4b2e1f45ff9bf21fec798c9dec9
ethonk <62923028+ethonk@users.noreply.github.com>
2025-01-24 22:18:41 +1300

    first push

---
+++ Tubboats/Source/Tubboats/Tubboats.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tubboats.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tubboats, "Tubboats" );
